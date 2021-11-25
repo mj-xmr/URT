971086f8b5c54533052359552dd4be577c8a5c22
mj-xmr <mjxmr@protonmail.com>
2021-11-25 09:01:27 +0100

    Build script for OSX

--- build/build-urt.sh
+++ build/build-urt.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Building... have a lot of patience."
+echo "Building URT... have a lot of patience."
 
 export PATH=/usr/lib/ccache:$PATH
 export CFLAGS="-O3 -march=native"
@@ -11,8 +11,17 @@ export CXXFLAGS=$CFLAGS
 cd "$DIR"
 make
 
-URT_INCLUDE=/usr/include/URT
+if [ "$(uname)" == "Darwin" ]; then
+	URT_INCLUDE=/opt/local/include/URT
+	URT_LIB=/opt/local/lib
+else
+	URT_INCLUDE=/usr/include/URT
+	URT_LIB=/usr/lib
+fi
+
+echo "Installing URT to: $URT_INCLUDE & $URT_LIB"
+
 sudo mkdir -p "$URT_INCLUDE"
-sudo cp ../lib/*.so* /usr/lib/
-sudo cp ../include/*.h* "$URT_INCLUDE"
+sudo cp -v ../lib/*.so* "$URT_LIB"
+sudo cp -v ../include/*.h* "$URT_INCLUDE"
 
